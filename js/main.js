298220a075d9a44ed611dc3f34453164bedaf1d2
Andrey <andriiano@yandex.ru>
2022-11-20 01:01:30 +0300

    add faq

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-$('.slider').slick();
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 300,
+    arrows: true,
+    // responsive: [
+    //     {
+    //       breakpoint: 1024,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 1,
+    //         infinite: true,
+    //         dots: true
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 600,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 2
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 480,
+    //       settings: {
+    //         slidesToShow: 1,
+    //         slidesToScroll: 1
+    //       }
+    //     }
+    //   ]
+});
+
